074d593c386e0e6c6344f0f49213b6a54ed13551
Jie Lin Lee <lin.leejie@gmail.com>
2022-07-30 14:41:34 +0800

    completed routes, controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :new, :create, :show ] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+category_arr = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: category_arr.sample,
+    )
+  5.times do
+    review = Review.new(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5)
+    )
+    review.restaurant = restaurant
+    review.save!
+  end
+    restaurant.save!
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
